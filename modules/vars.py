f7243952ba3c72c44ea7d646e9cb8cac66e44cf2
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-06-28 12:06:10 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,6 +1,6 @@
 # SudoR2spr WOODcraft
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 
-api_id = ""
-api_hash = ""
-bot_token = "64999--"
+api_id =27383453
+api_hash ="4c246fb0c649477cc2e79b6a178ddfaa"
+bot_token ="7171428829:AAGIf6PxLuLTv-Pq4zdvu9rbF3zNX9qgfZg"
